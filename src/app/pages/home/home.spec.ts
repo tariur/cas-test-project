f350bbf8c9757c403160eb1d4e198c8206787089
Martin Tari <tari.martin2002@gmail.com>
2025-11-24 13:48:00 +0100

    Start unit test setup

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Home } from './home';
+import { Auth } from '@angular/fire/auth';
+
 
 describe('Home', () => {
   let component: Home;
@@ -8,7 +10,8 @@ describe('Home', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [Home],
+      providers: [Auth]
     })
     .compileComponents();
 
